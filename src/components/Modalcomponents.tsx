f1f09a47bb55a7318d28e20252366f31a8999069
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-08-22 19:55:38 -0500

    prom

--- src/components/Modalcomponents.tsx
+++ src/components/Modalcomponents.tsx
@@ -22,7 +22,7 @@ function CommentWithImageModal() {
     setComment('');
     setImage(null);
   };
-
+ 
   return ( 
     <div className='p-[4px] rounded-2xl bg-web-color w-full '>
      <div className='flex items-center w-[722px] h-[75px]'>
